ec7b759b8a73f762448d8b1b95d021b368dd13f8
Aysan <asyababayeva18@mail.ru>
2024-07-31 10:00:35 +0300

    2 commit

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ var swiper = new Swiper('.swiper', {
     },
 });
 
+//Показать все/скрыть                
+
+        let showMoreBtn = document.querySelector('.show-more__btn')   //сохраняем в переменную кнопку
+        let sliderMenu = document.querySelector('.slider-menu')       //сохраняем в переменную класс с брендами 
+        let showMoreImg = document.querySelector('.show-more__img')   //сохраняем в переменную изо-е стрелочки 
+
+        showMoreBtn.addEventListener('click', function() {             //повесили обработчик событий на кнопку
+            if( showMoreBtn.innerHTML === 'Показать все') {            //если кнопка равна: показать все 
+                sliderMenu.style.maxHeight = 'max-content'             //высота контента макисмальная 
+                showMoreImg.style.transform = 'rotate(180deg)'         //стелочка вращется на 180 градусов
+                showMoreBtn.textContent = 'Скрыть'                     //текст кнопки теперь: скрыть 
+            } else {
+                sliderMenu.style.maxHeight = '140px'                    //иначе, высота контента 140px
+                showMoreImg.style.transform = 'rotate(0deg)'            //стрелочка в исходном положении
+                showMoreBtn.textContent = 'Показать все'                //текст кнопки: показать все
+            }
+        });
